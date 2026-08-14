714763f57b440be0c37c17ac2b84ab5b1897c125
Aviral Bhargava <aviralbhargava30@gmail.com>
2026-08-15 02:59:16 +0530

    Time: 18 ms (87.89%), Space: 12.6 MB (89.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,24 +2,31 @@ class Solution {
 public:
     int t[1001][1001];
     int lcs(string &x,string &y,int n , int m){
-         if(n==0|| m==0){
-            return 0;
+
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0||j==0){
+                     t[i][j]=0;
+                }
+            }
         }
-        if(t[n][m]!=-1){
-            return t[n][m];
-        }
-        if(x[n-1]==y[m-1]){
-            return t[n][m]=1+lcs(x,y,n-1,m-1);
-        }
-        else{
-            return t[n][m]=max(lcs(x,y,n-1,m),lcs(x,y,n,m-1));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(x[i-1]==y[j-1]){
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else{
+                    t[i][j]=max(t[i-1][j],t[i][j-1]);
+                }
+            }
+
         }
         return t[n][m];
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        memset(t,-1,sizeof(t));
+        
         int ans=lcs(text1,text2,n,m);
 
         return ans;
